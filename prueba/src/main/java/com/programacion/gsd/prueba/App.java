f333b127a8eebaec8888684b293a23bc581f9d69
david <davidcatasu@gmail.com>
2026-01-17 12:13:46 +0100

    Merge branch 'main' of git@github.com:Kata-technical/DAW-Entornos_de_desarrollo.git into main

---
+++ prueba/src/main/java/com/programacion/gsd/prueba/App.java
@@ -0,0 +1,10 @@
+package com.programacion.gsd.prueba;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
