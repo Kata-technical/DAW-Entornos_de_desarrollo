f0f6cfe4edb6aba9a60dde6569b3e1751594765b
david <davidcatasu@gmail.com>
2026-01-17 13:16:19 +0100

    cambios en app.java

--- prueba/src/main/java/com/programacion/gsd/prueba/App.java
+++ prueba/src/main/java/com/programacion/gsd/prueba/App.java
@@ -6,5 +6,7 @@ package com.programacion.gsd.prueba;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("COMO LAS HOJAS SE CAEN EN OTOÑO, TE COMERAN TOL COÑO");
+        String nigga = "the nigga";
     }
 }
